1635f294dd1097e2a7e0544e6401d6009a7fe1c2
Maxwell-Rosen <mrquell@gmail.com>
2023-12-07 16:06:20 -0500

    Initial commit: Building succeeds, but unit tests fail

--- src/_core.cpp
+++ src/_core.cpp
@@ -7,5 +7,6 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(_core, m) {
-  /// ... Fill me in!
+    py::class_<HarmonicOscillator> harmonicOscillator(m, "HarmonicOscillator");
+    harmonicOscillator.def(py::init<>());
 }
